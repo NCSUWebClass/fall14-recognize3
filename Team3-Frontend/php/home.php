6619e1ff9665ed9f22f8a315f1677355c5ce674b
eseidl <eseidl@ncsu.edu>
2014-12-11 14:57:27 -0500

    Frontend + API interaction

--- Team3-Frontend/php/home.php
+++ Team3-Frontend/php/home.php
@@ -1,6 +1,19 @@
 <?php
 $active_title = "Home";
 
+function get_galleries(){
+  $galleries = json_decode(file_get_contents('http://localhost/Team3-Recognize/galleries'));
+
+  foreach($galleries['galleries'] as $gallery){
+    echo "<a href=\"../php/question.php?=";
+    echo $gallery['name'];
+    echo " class=\"list-group-item\">";
+    echo $gallery['desc'];
+    echo  "</a>";
+  }
+}
+
+
 include './header.php';
 include '../content/header.html';
 include '../content/home.html';
