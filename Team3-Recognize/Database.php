b168cdb2e378db0792b3000b786b9187adae3653
Kshitij Parajuli <kshitijparajuli@gmail.com>
2014-12-11 13:17:02 -0500

    Databs

--- Team3-Recognize/Database.php
+++ Team3-Recognize/Database.php
@@ -16,9 +16,51 @@ class Database {
   public function __construct() {
     $this->connection = new PDO("mysql:host={$this->host};dbname={$this->dbname};charset=utf8", $this->username, $this->password);
   }
-  
+
   public function getConnection() {
     return $this->connection;
   }
 
+  private function selectStarTable($table) {
+    $stmt = $this->conn()->prepare("SELECT * from $table");
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+
+  public function viewGalleries() {
+    return selectStarTable("galleries");
+  }
+
+  public function viewGalleries() {
+    return selectStarTable("questions");
+  }
+
+  public function viewGallery($id) {
+    $stmt = $this->conn()->prepare("SELECT * from galleries WHERE id=:id");
+    $stmt->execute([
+      'id' => $id
+    ]);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+
+  public function viewQuestionsInGallery($gallery_id) {
+    $stmt = $this->conn()->prepare("SELECT * from questions WHERE gallery_id = :galerry_id");
+    $stmt->execute([
+      'gallery_id' => $gallery_id
+      ]);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+
+  public function viewQuestion($question_id) {
+    /// TODO Join with Answers table to get the right answer and other answers
+    $stmt = $this->conn()->prepare("SELECT * from questions WHERE id = :qid");
+    $stmt->execute([
+      'qid' => $question_id
+      ]);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $rows;
+  }
 }
--- Team3-Recognize/index.php
+++ Team3-Recognize/index.php
@@ -1,12 +1,14 @@
 <?php
 require 'vendor/autoload.php';
+require 'Database.php';
 
 $app = new \Slim\Slim();
 $app->view(new \JsonApiView());
 $app->add(new \JsonApiMiddleware());
+$db = new Database();
 
 $app->get('/galleries', function() use ($app) {
-  // TODO: fetch this from the DB
+
   $app->render(200, [
     'galleries' =>
       [
